512e359c4cd57daba529f9797b6ddbe1ef8b867c
shubhRepository <academicShubh@gmail.com>
2021-12-30 14:16:04 +0530

    test

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+
 require("./startup/config")();
 require("./startup/db")();
 require("./startup/routes")(app);
-app.listen(5000, () => console.log("lisiting at port 5000"));
+app.listen(5000, () => console.log("Listening to port 5000"));
