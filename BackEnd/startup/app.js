f96db74681c55b659b1dea173da8dcb751e0c784
shubhRepository <academicShubh@gmail.com>
2022-01-01 23:06:14 +0530

    all unhandled routes handler

--- BackEnd/startup/app.js
+++ BackEnd/startup/app.js
@@ -5,16 +5,22 @@ const userRouter = require("../routes/userRouter");
 const managers = require("../routes/managers");
 const images = require("../routes/images");
 
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use("/login", auth);
 // app.use("/signup", users);
-app.use("/api",images)
+app.use("/api", images);
 app.use("/users", userRouter);
-app.use("/users/managers",managers)
+app.use("/users/managers", managers);
 
+// for all unhandled routes
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
