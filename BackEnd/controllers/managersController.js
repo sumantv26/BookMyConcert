80cf2e04b05bee8ff771f3d916aa7937dede7d45
sumantv26 <sumantv26@gmail.com>
2022-01-02 12:17:04 +0530

    test

--- BackEnd/controllers/managersController.js
+++ BackEnd/controllers/managersController.js
@@ -3,6 +3,6 @@ const Manager = require("../models/Manager");
 module.exports.getApprovedMangers = async (req, res, next) => {
   const managers = await Manager.find({ isApproved: true }).select("-password");
 };
-// i am changing this
+// i am changing this so you can see
 // hello sumant how are you?
-//changes 2.0
+//
