b4ecaf608df9a53df14c3f4d4c1388c1e4ab3173
Shubham Maheshwari <academicShubh@gmail.com>
2022-01-06 17:03:05 +0530

    validate signup systematically

--- BackEnd/middleware/ValidateInputFields.js
+++ BackEnd/middleware/ValidateInputFields.js
@@ -1,26 +1,4 @@
 const Joi = require("joi");
-// module.exports = (req, res, next) => {
-//   const schema = Joi.object({
-//     email: Joi.string().email().required(),
-//     password: Joi.string().min(3).max(15).required(),
-//     role: Joi.string().valid("user", "customer manager", "admin"),
-//   });
-//   const { error } = schema.validate(req.body);
-//   if (error) return res.status(400).send(error.message);
-
-//   next();
-// };
-const validateManager = (data) => {
-  const managerSchema = Joi.object({
-    // contactNumber: Joi.string()
-    //   .length(10)
-    //   .pattern(/^[0-9]+$/)
-    //   .required(),
-    // creditCardNum: Joi.string().length(16).required(),
-    // isApproved: Joi.boolean().default(false),
-  }).options({ allowUnknown: true });
-  return managerSchema.validate(data);
-};
 
 exports.validateSignup = (req, res, next) => {
   const user = req.body;
@@ -28,27 +6,38 @@ exports.validateSignup = (req, res, next) => {
   // 18 years ago date
   const cutOffDate = new Date(now - 1000 * 60 * 60 * 24 * 365 * 18);
 
-  const commonSchema = Joi.object({
+  const commonSchema = {
     name: Joi.string().min(3).max(255).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(3).max(15).required(),
-    // birthDate: Joi.date().max(cutOffDate).required(),
-    // gender: Joi.string().valid("male", "female", "other"),
-    role: Joi.string().valid("customer", "manager", "admin").required(),
-  }).options({ allowUnknown: true });
+    passwordConfirmation: Joi.any()
+      .equal(Joi.ref("password"))
+      .required()
+      .label("Confirm password")
+      .options({ messages: { "any.only": "{{#label}} does not match" } }),
+    birthDate: Joi.date().max(cutOffDate).required(),
+    gender: Joi.string().valid("male", "female", "other").required(),
+    role: Joi.string().valid("user", "manager", "admin").required(),
+  };
 
-  const { error } = commonSchema.validate(user);
+  const managerSchema = {
+    ...commonSchema,
+    contactNumber: Joi.string()
+      .length(10)
+      .pattern(/^[0-9]+$/)
+      .required(),
+    creditCardNum: Joi.string().length(16).required(),
+    isApproved: Joi.boolean().default(false),
+  };
+
+  let validationSchema;
+  if (user.role === "manager")
+    validationSchema = Joi.object({ ...managerSchema });
+  else validationSchema = Joi.object({ ...commonSchema });
+
+  const { error } = validationSchema.validate(user);
   if (error) return res.status(400).send(error.message);
 
-  if (user.role === "manager") {
-    const { error } = validateManager(user);
-    if (error) return res.status(400).send(error.message);
-  }
-
-  // if (!schema.validate(customer)) {
-  //   const { error } = validate(data);
-  //   if (error) return res.status(400).send(error.message);
-  // }
   next();
 };
 
